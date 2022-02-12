254b27a682738c3c2152f00e1d903578ffe951cc
Lucas <lucas.huenniger02@gmail.com>
2022-02-12 14:19:26 +0100

    implemented the method destroy() and renamed all methods in a simplier fomat

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,6 +1,33 @@
 #include "BinaryTree.h"
+#include <QDebug>
 
-void BinaryTree::createNewNode(const int data)
+void BinaryTree::create(const int data)
 {
     Node* tempNode = new Node(data);
+}
+
+void BinaryTree::display(Node* leaf)
+{
+    // no leaf anymore
+    if (leaf == nullptr)
+    {
+        //qDebug() << "This is an empty binary tree. Add Nodes with .createNewNode(data)";
+        return;
+    }
+
+    if (leaf != nullptr)
+    {
+        display(leaf->leftNode);
+        display(leaf->rightNode);
+    }
+}
+
+void BinaryTree::destroy(Node* leaf)
+{
+    if (leaf != nullptr)
+    {
+        destroy(leaf->leftNode);
+        destroy(leaf->rightNode);
+        delete leaf;
+    }
 }
--- BinaryTree.h
+++ BinaryTree.h
@@ -18,13 +18,12 @@ struct Node
 class BinaryTree
 {
 public:
-    BinaryTree();
-    ~BinaryTree();
-
-    void createNewNode(const int data);
-
-private:
-    Node* m_rootNode = nullptr;
+    // creates a new Node in the binary tree
+    void create(const int data);
+    // displays all nodes in form of a tree --> @TODO: adding this function after we had this in the course (recursive is easier i would guess)
+    //void display(Node* rootNode);
+    // destroys the tree
+    void destroy(Node* leaf);
 };
 
 #endif // BINARYTREE_HFF9A3BCAB8CB4554B280D13439C41715
