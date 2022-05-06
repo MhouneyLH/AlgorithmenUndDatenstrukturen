5405d3c9c287c4a272e640a39ffca185ab9939f0
Lucas Hünniger <lucas.huenniger02@gmail.com>
2022-05-06 20:37:03 +0200

    add the prototypes of every needed function (constructor already finished)

--- Stack.cpp
+++ Stack.cpp
@@ -1,6 +1,34 @@
 #include "Stack.h"
 
-Stack::Stack()
+Stack::Stack(const int maxSize, const int top, char* items)
+    : m_maxSize(maxSize)
+    , m_top(top)
+    , m_items(items)
 {
+}
 
+Stack::~Stack()
+{
+    m_items = nullptr;
+    delete m_items;
+}
+
+void Stack::push(Stack* stack, const char data)
+{
+}
+
+char Stack::peak(Stack* stack)
+{
+}
+
+char Stack::pop(Stack* stack)
+{
+}
+
+bool Stack::isEmpty(Stack* stack)
+{
+}
+
+bool Stack::isFull(Stack* stack)
+{
 }
--- Stack.h
+++ Stack.h
@@ -4,7 +4,20 @@
 class Stack
 {
 public:
-    Stack();
+    Stack(const int maxSize, const int top, char* items);
+    ~Stack();
+
+    void push(Stack* stack, const char data);
+    char peak(Stack* stack);
+    char pop(Stack* stack);
+
+    bool isEmpty(Stack* stack);
+    bool isFull(Stack* stack);
+
+private:
+    int m_maxSize = 10;
+    int m_top = -1;
+    char* m_items = nullptr;
 };
 
 #endif // STACK_H4C16A066861F4E1690C63D6EC0DA3E31
